af2f56a1dbebdefe635a816823a028789b077c0d
attinder-shf <attinder@superhifi.com>
2022-08-29 15:15:17 -0700

    package.swift updated

--- Package.swift
+++ Package.swift
@@ -1,11 +1,19 @@
 // swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-
 import PackageDescription
 
 let package = Package(
     name: "AsyncTimer",
     dependencies: [],
-    exclude: ["Example"]
+    targets: [
+        .target(
+            name: "AsyncTimer IOS",
+            path: "Source"
+        ),
+        .target(
+            name: "AsyncTimer macOS",
+            path: "Source"
+        )
+    ]
 )
